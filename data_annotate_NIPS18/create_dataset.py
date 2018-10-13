a8b8a1570bac18859b04c8bd9461d0111b78142a
lshao <laetitia.shao@gmail.com>
2018-10-12 17:04:42 -0700

    Modify script to take into account missing label (dont include in the dataset in this case

--- data_annotate_NIPS18/create_dataset.py
+++ data_annotate_NIPS18/create_dataset.py
@@ -8,7 +8,7 @@ import numpy as np
 import csv
 
 DATA_ROOT= 'data/'
-CSV_FILE = "videos2.csv"
+CSV_FILE = "videos_additional.csv"
 RESULT_ROOT = "result"
 csv_columns = ['Video_name', 'Frame', 'Image_path', 'Label']
 pathlist = Path('data/''').glob('**/*.jpg')
@@ -16,7 +16,15 @@ pathlist = Path('data/''').glob('**/*.jpg')
 
 # Aggregate labels from all CSV files
 labels_dict = {}
-result_folders = os.listdir(RESULT_ROOT)
+# result_folders = os.listdir(RESULT_ROOT)
+result_folders = [
+    'results64_17-06-05',
+    'results64_17-06-07',
+    'results64_17-06-12',
+    'results64_17-06-13',
+    'results64_17-06-15'
+]
+
 for folder in result_folders:
     if os.path.isdir(os.path.join(RESULT_ROOT, folder)):
         print(folder)
@@ -27,25 +35,17 @@ for folder in result_folders:
             num_csvs = len(result_csvs)
             labels_dict[camera] = {} # dict frame index -> label because labels might not be in order within the file
             for i in range(num_csvs):
-                with open(os.path.join(result_folder, str(i) + '.csv'), 'r') as csvfile:
-                    labelreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-                    for row in labelreader:
-                        if len(row) == 3:
-                            for j in range(int(row[0]), int(row[1]) + 1):
-                                labels_dict[camera][j + 500 * i] = int(row[2])
-
-labels = {} # frame name to array
-for k, v in labels_dict.items():
-    labels[k] = []
-    for i in range(len(v.keys())):
-        print (i, i in v.keys())
-        if i not in v.keys():
-            print("Error in ", k)
-        labels[k].append(v[i])
+                if os.path.isfile(os.path.join(result_folder, str(i) + '.csv')):
+                    with open(os.path.join(result_folder, str(i) + '.csv'), 'r') as csvfile:
+                        labelreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+                        for row in labelreader:
+                            if len(row) == 3:
+                                for j in range(int(row[0]), int(row[1]) + 1):
+                                    labels_dict[camera][j + 500 * i] = int(row[2])
 
 # Aggregate file names for all labeled data:
 frames = {}
-for name in labels.keys():
+for name in labels_dict.keys():
     details = name.split("_")
     date = details[0]
     camera = details[1]
@@ -61,17 +61,18 @@ with open(CSV_FILE, "w") as data_file:
     writer = csv.DictWriter(data_file, fieldnames=csv_columns, delimiter=' ')
     writer.writeheader()
     for k, v in frames.items():
-        assert(len(v) == len(labels[k])), "Number of labels and images don't match for video {}: {} v. {}".format(
-            k,
-            len(v),
-            len(labels[k])
-        )
+        # assert(len(v) == len(labels[k])), "Number of labels and images don't match for video {}: {} v. {}".format(
+        #     k,
+        #     len(v),
+        #     len(labels[k])
+        # )
         details = k.split("_")
         date = details[0]
         camera = details[1]
         for (i, x) in enumerate(v):
-            writer.writerow({
-                'Video_name': k,
-                'Frame': i,
-                'Image_path': os.path.join(date, "10.233.219." + camera, x),
-                'Label': labels[k][i]})
+            if i in labels_dict[k]:
+                writer.writerow({
+                    'Video_name': k,
+                    'Frame': i,
+                    'Image_path': os.path.join(date, "10.233.219." + camera, x),
+                    'Label': labels_dict[k][i]})
